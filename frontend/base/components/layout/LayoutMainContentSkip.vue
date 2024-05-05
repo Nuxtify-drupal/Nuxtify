04f488063617a04aa476ac2daad7829db4e8f947
Connor van Spronssen <info@dobefu.com>
2024-05-05 17:57:00 +0200

    Move paragraphs to server components

--- frontend/base/components/layout/LayoutMainContentSkip.vue
+++ frontend/base/components/layout/LayoutMainContentSkip.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <Teleport to="body">
     <a
       href="#main-content"
       class="absolute z-50 p-2 text-center -translate-x-1/2 left-1/2 -top-16 focus:top-0"
     >
-      {{ $t('skip_to_main_content') }}
+      {{ t('skip_to_main_content') }}
     </a>
   </Teleport>
 </template>
