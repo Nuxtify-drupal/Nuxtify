41a7f9a3cd0cfd41cb258678110f27cf09f03b72
Connor van Spronssen <info@dobefu.com>
2024-05-05 16:20:23 +0200

    Add logic to require the API token to be verfied in order to invalidate caches

--- frontend/base/server/api/cache/invalidate.post.ts
+++ frontend/base/server/api/cache/invalidate.post.ts
@@ -1,5 +1,12 @@
+import process from 'node:process'
+
 export default defineEventHandler(async (event) => {
   const { tags } = await readBody(event)
+  const { headers } = event
+  const token = headers.get('token') ?? ''
+
+  if (token !== process.env.BACKEND_API_KEY)
+    return new Response('Unauthorized', { status: 401 })
 
   const storage = useStorage('cacheData')
   const storageKeys = await storage.getKeys()
